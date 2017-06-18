faa30cd978216471125e265b021c272b11b0c6d6
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-06-19 04:11:30 +0700

    remove unused package

--- layout/src/Padding.js
+++
@@ -1,8 +0,0 @@
-export const Padding = styled.div`
-	padding: ${props => props.padding};
-	padding: ${props => [props.top, props.right, props.bottom, props.left]
-		.map(e => String(e || 0) + (Number.isInteger(e) ? 'px' : ''))
-		.join(' ')};
-	padding: ${props => String(props.all) + (Number.isInteger(props.all) ? 'px' : '')};
-
-`;
