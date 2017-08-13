8c60d9d71104df6698c327b393ec733d43eb0f0e
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-08-13 14:16:24 +0700

    fix typeof check

--- src/history.js
+++ src/history.js
@@ -1,6 +1,6 @@
 import createHistory from "history/createBrowserHistory";
 let history = {}
-if (typeof window !== undefined) {
+if (typeof window !== 'undefined') {
 	history = createHistory();
 }
 export default history
