acc06f81323e64b3c9a1d5794e2ee49952f84dcd
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-11-17 11:03:00 +0700

    support editing in codesandbox.io

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import { AsyncList, Form, Link, history, styled, createState } from '../../src';
+import { AsyncList, Form, Link, history, styled, createState } from "../../src";
 
-const height = '2.5em';
+const height = "2.5em";
 
-const App = styled('div')`
+const App = styled("div")`
   padding: 1em;
   padding-top: 4em;
   font-family: Roboto, sans-serif;
 `;
 
-const Header = styled('div')`
+const Header = styled("div")`
   position: fixed;
   top: 0;
   left: 0;
@@ -23,10 +23,10 @@ const Header = styled('div')`
   display: flex;
   min-height: 64px;
   align-items: center;
-  @media (max-width: 768px){
+  @media (max-width: 768px) {
     background: pink;
   }
-  @media (max-width: 320px){
+  @media (max-width: 320px) {
     background: red;
   }
 `;
@@ -36,13 +36,13 @@ const HeaderLink = styled(Link)`
   padding: 1em;
 `;
 
-const Fieldset = styled('fieldset')`
+const Fieldset = styled("fieldset")`
   padding: 1em;
   border: 1px solid #eee;
   border-radius: 2px;
 `;
 
-const Input = styled('input')`
+const Input = styled("input")`
   display: block;
   height: ${height};
   font-size: inherit;
@@ -51,7 +51,7 @@ const Input = styled('input')`
   width: 100%;
 `;
 
-const Button = styled('button')`
+const Button = styled("button")`
   display: block;
   width: 100%;
   height: ${height};
@@ -77,96 +77,92 @@ const showFile = (key, value) =>
         lastModifiedDate: value.lastModifiedDate,
         name: value.name,
         size: value.size,
-        type: value.type,
+        type: value.type
       }
     : value;
 
-const renderFieldset = (state, bind) =>
+const renderFieldset = (state, bind) => (
   <Fieldset style={{ opacity: state.loading ? 0.5 : 1 }}>
-    <Input {...bind('name')} type="text" />
-    <Input {...bind(['email', 0])} type="text" />
+    <Input {...bind("name")} type="text" />
+    <Input {...bind(["email", 0])} type="text" />
     <Input
       onChange={e =>
-        bind(['profile', 'avatar']).onChange({
+        bind(["profile", "avatar"]).onChange({
           target: {
             name: e.target.name,
-            value: e.target.files[0],
-          },
+            value: e.target.files[0]
+          }
         })}
       type="file"
     />
     <Button>Submit</Button>
-    <pre>
-      {console.log(state) || JSON.stringify(state, showFile, 2)}
-    </pre>
-  </Fieldset>;
+    <pre>{console.log(state) || JSON.stringify(state, showFile, 2)}</pre>
+  </Fieldset>
+);
 
-const ReusableForm = props =>
+const ReusableForm = props => (
   <div>
     <h1>Reusable Form Demo</h1>
     <Form
       onSubmit={handleSubmit}
       formData={{
-        email: ['test@test.com'],
+        email: ["test@test.com"]
       }}
     >
       {renderFieldset}
     </Form>
-  </div>;
+  </div>
+);
 
-const Box = styled('div')`
-  padding: 1em;
-`;
+const Box = styled("div")`padding: 1em;`;
 
-const ReusableFetch = props =>
+const urls = [
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/posts"
+];
+
+const ReusableFetch = props => (
   <div>
     <h1>Reusable Fetch Demo</h1>
     <Box>
       <h2>Multiple URL</h2>
       <AsyncList
-        url={[
-          'https://jsonplaceholder.typicode.com/posts',
-          'https://jsonplaceholder.typicode.com/posts',
-          'https://jsonplaceholder.typicode.com/posts',
-          'https://jsonplaceholder.typicode.com/posts',
-          'https://jsonplaceholder.typicode.com/posts',
-          'https://jsonplaceholder.typicode.com/posts',
-          'https://jsonplaceholder.typicode.com/posts',
-        ]}
-        onLoading={props =>
-          <p>
-            Loading {props.url.join(', ')}
-          </p>}
+        url={urls}
+        onLoading={props => <p>Loading {props.url.join(", ")}</p>}
       />
     </Box>
     <Box>
       <h2>onSuccess</h2>
-      <AsyncList url={'https://jsonplaceholder.typicode.com/posts'} />
+      <AsyncList url={"https://jsonplaceholder.typicode.com/posts"} />
     </Box>
     <Box>
       <h2>Cached</h2>
-      <AsyncList url={'https://jsonplaceholder.typicode.com/posts'} cache />
+      <AsyncList url={"https://jsonplaceholder.typicode.com/posts"} cache />
     </Box>
     <Box>
       <h2>onError</h2>
       <AsyncList
-        url={'https://jsonplaceholder.typicode.com/poss'}
-        onError={(error, reload) =>
+        url={"https://jsonplaceholder.typicode.com/poss"}
+        onError={(error, reload) => (
           <div>
             <p>Error!</p>
-            <pre>
-              {error.message}
-            </pre>
+            <pre>{error.message}</pre>
             <Button
               onClick={e =>
                 reload({
                   ...props,
-                  url: 'https://jsonplaceholder.typicode.com/posts',
+                  url: "https://jsonplaceholder.typicode.com/posts"
                 })}
             >
               Reload?
             </Button>
-          </div>}
+          </div>
+        )}
       />
     </Box>
     <Box>
@@ -175,47 +171,45 @@ const ReusableFetch = props =>
     </Box>
     <Box>
       <h2>With Children</h2>
-      <AsyncList url={'https://jsonplaceholder.typicode.com/posts'}>
-        {(data, state, props) =>
-          <pre style={{ whiteSpace: 'pre-wrap' }}>
+      <AsyncList url={"https://jsonplaceholder.typicode.com/posts"}>
+        {(data, state, props) => (
+          <pre style={{ whiteSpace: "pre-wrap" }}>
             {JSON.stringify(data, null, 2)}
-          </pre>}
+          </pre>
+        )}
       </AsyncList>
     </Box>
-  </div>;
+  </div>
+);
 
 const state = {
   counter: 0,
   decrement: () =>
     state.set({
-      counter: state.counter - 1,
+      counter: state.counter - 1
     }),
   increment: () =>
     state.set({
-      counter: state.counter + 1,
-    }),
+      counter: state.counter + 1
+    })
 };
 
 const withState = createState(state);
 
-const Center = styled('div')`
-  text-align: center;
-`
+const Center = styled("div")`text-align: center;`;
 
-const Counter = withState(state =>
+const Counter = withState(state => (
   <Center>
-    <h1>
-      {state.counter}
-    </h1>
+    <h1>{state.counter}</h1>
     <button onClick={state.increment}>Increment</button>
     <button onClick={state.decrement}>Decrement</button>
   </Center>
-);
+));
 
 const renderApp = ({
   history: { location: { pathname } },
   is = (path, exact) =>
-    exact ? path === pathname : pathname.match(new RegExp(path)),
+    exact ? path === pathname : pathname.match(new RegExp(path))
 }) =>
   render(
     <App>
@@ -225,12 +219,12 @@ const renderApp = ({
         <HeaderLink to="/fetch">Fetch</HeaderLink>
         <HeaderLink to="/state">State</HeaderLink>
       </Header>
-      {is('/', true) && <h1>Welcome to Reusable</h1>}
-      {is('/form') && <ReusableForm />}
-      {is('/fetch') && <ReusableFetch />}
-      {is('/state') && <Counter />}
+      {is("/", true) && <h1>Welcome to Reusable</h1>}
+      {is("/form") && <ReusableForm />}
+      {is("/fetch") && <ReusableFetch />}
+      {is("/state") && <Counter />}
     </App>,
-    document.querySelector('#demo')
+    document.querySelector("#root")
   );
 
 renderApp({ history });
